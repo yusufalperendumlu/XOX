7b06a5c609080810f7a86adc73300125ebd1b317
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-01-04 21:28:40 +0300

    xox

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import { Game } from "./page/Game";
+
+const App = () => {
+  return (
+    <div>
+      <Game />
+    </div>
+  );
+};
+
+export default App;
